8a48943d08d1132c7bc5c43976756358c11bbb1e
Frcgg <sevillanorichar266@gmail.com>
2025-04-10 16:04:28 -0500

    Ya funciona el link para el admin

--- proyecto-pasto/frontend/src/App.jsx
+++ proyecto-pasto/frontend/src/App.jsx
@@ -1,43 +1,60 @@
+import React from 'react';
 import { BrowserRouter as Router, Routes, Route, Link } from 'react-router-dom';
 import SobrePasto from './components/SobrePasto';
 import SobreCarnaval from './components/SobreCarnaval';
-import Introduccion from './components/Introduccion'; // Importa el componente Introduccion
+import Introduccion from './components/Introduccion';
+//import Alojamientos from './components/GuiaTuristico/Alojamientos';
+//import InfoContacto from './components/GuiaTuristico/InfoContacto';
+//import PanelPersonal from './components/GuiaTuristico/PanelPersonal';
+//import PuntosDeInteres from './components/GuiaTuristico/PuntosDeInteres';
+//import Restaurantes from './components/GuiaTuristico/Restaurantes';
+import RutasTuristicas from './components/GuiaTuristico/RutasTuristicas';
+import AdminPanel from './components/Admin/Adminpanel'; // Importa el componente AdminPanel
+
 
 function App() {
-    return (
-        <Router>
-            <nav className="navbar navbar-expand-lg navbar-light bg-light">
-                <div className="container-fluid">
-                    <div className="d-flex justify-content-center">
-                        <Link className="nav-link" to="/">Inicio</Link>
-                    </div>
-                    <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
-                        <span className="navbar-toggler-icon"></span>
-                    </button>
-                    <div className="collapse navbar-collapse" id="navbarNav">
-                        <ul className="navbar-nav ms-auto me-auto mb-2 mb-lg-0">
-                            <li className="nav-item">
-                                <Link className="nav-link" to="/sobre-pasto">Sobre Pasto</Link>
-                            </li>
-                            <li className="nav-item">
-                                <Link className="nav-link" to="/sobre-carnaval">Sobre el Carnaval</Link>
-                            </li>
-                            <li className="nav-item">
-                                <Link className="nav-link" to="/Iniciar Sesion">Iniciar Sesion</Link>
-                            </li>
-                        </ul>
-                    </div>
-                </div>
-            </nav>
-    
-            {/* Rutas */}
-            <Routes>
-                <Route path="/sobre-pasto" element={<SobrePasto />} />
-                <Route path="/sobre-carnaval" element={<SobreCarnaval />} />
-                <Route path="/" element={<Introduccion />} /> // Muestra la introducción en la página principal
-            </Routes>
-        </Router>
-    );
+  return (
+    <Router>
+      <nav className="navbar navbar-expand-lg navbar-light bg-light">
+        <div className="container-fluid">
+          <div className="d-flex justify-content-center">
+            <Link className="nav-link" to="/">Inicio</Link>
+          </div>
+          <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+            <span className="navbar-toggler-icon"></span>
+          </button>
+          <div className="collapse navbar-collapse" id="navbarNav">
+            <ul className="navbar-nav ms-auto me-auto mb-2 mb-lg-0">
+              <li className="nav-item">
+                <Link className="nav-link" to="/sobre-pasto">Sobre Pasto</Link>
+              </li>
+              <li className="nav-item">
+                <Link className="nav-link" to="/sobre-carnaval">Sobre el Carnaval</Link>
+              </li>
+              <li className="nav-item">
+                <Link className="nav-link" to="/guia-turistico">Guía Turístico</Link>
+              </li>
+              <li className="nav-item">
+                <Link className="nav-link" to="/admin">Admin Panel</Link>
+              </li>
+              <li className="nav-item">
+                <Link className="nav-link" to="/iniciar-sesion">Iniciar Sesión</Link>
+              </li>
+            </ul>
+          </div>
+        </div>
+      </nav>
+
+      <Routes>
+        <Route path="/sobre-pasto" element={<SobrePasto />} />
+        <Route path="/sobre-carnaval" element={<SobreCarnaval />} />
+        <Route path="/" element={<Introduccion />} />
+        <Route path="/admin" element={<AdminPanel />} /> {/* Ruta para el AdminPanel */}
+        <Route path="/guia-turistico" element={<RutasTuristicas />} />
+
+      </Routes>
+    </Router>
+  );
 }
 
 export default App;
