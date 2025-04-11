b6420eaf974584ffdf55f2498511c84683f21a25
Frcgg <sevillanorichar266@gmail.com>
2025-04-11 01:12:55 -0500

    YA PUSE EL "QUE HACER" JOSE

--- proyecto-pasto/frontend/src/App.jsx
+++ proyecto-pasto/frontend/src/App.jsx
@@ -5,59 +5,99 @@ import SobreCarnaval from './components/SobreCarnaval';
 import Introduccion from './components/Introduccion';
 import Alojamientos from './components/GuiaTuristico/Alojamientos';
 import InfoContacto from './components/GuiaTuristico/InfoContacto';
-//import PanelPersonal from './components/GuiaTuristico/PanelPersonal';
-//import PuntosDeInteres from './components/GuiaTuristico/PuntosDeInteres';
-//import Restaurantes from './components/GuiaTuristico/Restaurantes';
-//import RutasTuristicas from './components/GuiaTuristico/RutasTuristicas';
-import AdminPanel from './components/Admin/Adminpanel'; // Importa el componente AdminPanel
-import navbarBackground from "../public/Img_Video/navbar.jpg"; // Ajusta la ruta según tu estructura
-
+import AdminPanel from './components/Admin/Adminpanel';
+import navbarBackground from "../public/Img_Video/navbar.jpg";
+import CarnivalDays from './components/CarnivalDays';
+import './App.css'; // Asegúrate de importar tu archivo CSS
 
 function App() {
   return (
-
     <Router>
-      <nav className="navbar navbar-expand-lg navbar-light" style={{ backgroundImage: `url(${navbarBackground})`, backgroundSize: 'cover', backgroundPosition: 'center', color: 'white' }}>
+      <nav
+        className="navbar navbar-expand-lg navbar-light"
+        style={{
+          backgroundImage: `url(${navbarBackground})`,
+          backgroundSize: 'cover',
+          backgroundPosition: 'center',
+          color: 'white',
+        }}
+      >
         <div className="container-fluid">
           <div className="d-flex justify-content-center">
-            <Link className="nav-link" to="/">Inicio</Link>
+            <Link className="nav-link" to="/">
+              Inicio
+            </Link>
           </div>
-          <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+          <button
+            className="navbar-toggler"
+            type="button"
+            data-bs-toggle="collapse"
+            data-bs-target="#navbarNav"
+            aria-controls="navbarNav"
+            aria-expanded="false"
+            aria-label="Toggle navigation"
+          >
             <span className="navbar-toggler-icon"></span>
           </button>
           <div className="collapse navbar-collapse" id="navbarNav">
             <ul className="navbar-nav ms-auto me-auto mb-2 mb-lg-0">
-              <li className="nav-item" >
-                <Link className="nav-link" to="/sobre-pasto"  style={{ color: 'white' }} >Sobre Pasto</Link>
+              <li className="nav-item">
+                <Link
+                  className="nav-link"
+                  to="/sobre-pasto"
+                  style={{ color: 'white' }}
+                >
+                  Sobre Pasto
+                </Link>
               </li>
               <li className="nav-item">
-                <Link className="nav-link" to="/sobre-carnaval" style={{ color: 'white' }}>Sobre el Carnaval</Link>
+                <Link
+                  className="nav-link"
+                  to="/sobre-carnaval"
+                  style={{ color: 'white' }}
+                >
+                  Sobre el Carnaval
+                </Link>
               </li>
               <li className="nav-item">
-                <Link className="nav-link" to="/guia-turistico"></Link>
+                <Link className="nav-link" to="/carnival-days" style={{ color: 'white' }}> 
+                  ¿Que hacer?
+                </Link>
               </li>
               <li className="nav-item">
-                <Link className="nav-link" to="/admin"></Link>
+                <Link className="nav-link" to="/guia-turistico">
+                  
+                </Link>
               </li>
               <li className="nav-item">
-                <Link className="nav-link" to="/iniciar-sesion"></Link>
+                <Link className="nav-link" to="/admin">
+                  
+                </Link>
+              </li>
+              <li className="nav-item">
+                <Link className="nav-link" to="/iniciar-sesion">
+                  
+                </Link>
               </li>
             </ul>
           </div>
         </div>
       </nav>
 
-
       <Routes>
         <Route path="/sobre-pasto" element={<SobrePasto />} />
         <Route path="/sobre-carnaval" element={<SobreCarnaval />} />
         <Route path="/" element={<Introduccion />} />
-        <Route path="/admin" element={<AdminPanel />} /> {/* Ruta para el AdminPanel */}
-        <Route path="guia-turistico" element={<InfoContacto />} />
-
+        <Route path="/admin" element={<AdminPanel />} />
+        <Route path="/guia-turistico" element={<InfoContacto />} />
+        <Route
+          path="/carnival-days"
+          element={<CarnivalDays className="carnival-days-page" />}
+        />
       </Routes>
     </Router>
   );
 }
 
 export default App;
+
---
+++ proyecto-pasto/frontend/src/components/CarnivalDays.jsx
@@ -0,0 +1,136 @@
+import React from 'react';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import './StyleCarnaval.css';
+
+function CarnivalDays() {
+  return (
+    <div className="container">
+      <h1 className="text-center">DÍAS DE CARNAVAL</h1>
+      <div className="row">
+        <div className="col-md-4">
+          <div className="card">
+            <img
+              src="../public/Img_Video/precarn.jpg"
+              className="card-img-top custom-image"
+              alt="Pre Carnaval"
+            />
+            <div className="card-body">
+              <h5 className="card-title">PRE CARNAVAL</h5>
+              <p className="card-text">
+                Conciertos, Arco Iris en el asfalto, Carreras y recorridos,
+                actividades en cada comuna de la ciudad forman parte de la
+                agenda pre-carnaval.
+              </p>
+              <a href="#" className="btn btn-primary">
+                Programación
+              </a>
+            </div>
+          </div>
+        </div>
+        <div className="col-md-4">
+          <div className="card">
+            <img
+              src="../public/Img_Video/carnavalito.jpg"
+              className="card-img-top custom-image"
+              alt="2 de Enero"
+            />
+            <div className="card-body">
+              <h5 className="card-title">2 DE ENERO</h5>
+              <p className="card-text">
+                Se celebra el carnavalito, el lugar para los niños dentro del
+                carnaval, inician los tablados y festivales en plazas públicas.
+              </p>
+              <a href="#" className="btn btn-primary">
+                Programación
+              </a>
+            </div>
+          </div>
+        </div>
+        <div className="col-md-4">
+          <div className="card">
+            <img
+              src="../public/Img_Video/colectivos.jpg"
+              className="card-img-top custom-image"
+              alt="3 de Enero"
+            />
+            <div className="card-body">
+              <h5 className="card-title">3 DE ENERO</h5>
+              <p className="card-text">
+                El espectacular desfile de colectivos coreográficos recuerda las
+                travesías épicas en tributo a la madre tierra y el pasado
+                ancestral andino.
+              </p>
+              <a href="#" className="btn btn-primary">
+                Programación
+              </a>
+            </div>
+          </div>
+        </div>
+      </div>
+      <div className="row mt-4">
+        <div className="col-md-4">
+          <div className="card">
+            <img
+              src="../public/Img_Video/castañeda.jpg"
+              className="card-img-top custom-image"
+              alt="4 de Enero"
+            />
+            <div className="card-body">
+              <h5 className="card-title">4 DE ENERO</h5>
+              <p className="card-text">
+                La llegada de la Familia Castañeda, un desfile tradicional con
+                pasajes de identidad, hechos y personajes de la memoria rural y
+                urbana de la región.
+              </p>
+              <a href="#" className="btn btn-primary">
+                Programación
+              </a>
+            </div>
+          </div>
+        </div>
+        <div className="col-md-4">
+          <div className="card">
+            <img
+              src="../public/Img_Video/5enero.jpg"
+              className="card-img-top custom-image"
+              alt="5 de Enero"
+            />
+            <div className="card-body">
+              <h5 className="card-title">5 DE ENERO</h5>
+              <p className="card-text">
+                Juego de identidad, práctica lúdica que dio origen al juego de
+                carnaval, es sentir la alegría al compartir el ritual de
+                pintarse el rostro.
+              </p>
+              <a href="#" className="btn btn-primary">
+                Programación
+              </a>
+            </div>
+          </div>
+        </div>
+        <div className="col-md-4">
+          <div className="card">
+            <img
+              src="../public/Img_Video/6enero.jpg"
+              className="card-img-top custom-image"
+              alt="6 de Enero"
+            />
+            <div className="card-body">
+              <h5 className="card-title">6 DE ENERO</h5>
+              <p className="card-text">
+                El Desfile Magno, un día apoteósico donde la creatividad se
+                siente por toda la senda; disfraces, comparsas y carrozas
+                emergen e inundan la ciudad.
+              </p>
+              <a href="#" className="btn btn-primary">
+                Programación
+              </a>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default CarnivalDays;
