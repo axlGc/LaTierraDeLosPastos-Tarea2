c05af712e103367662b3f8ce9f1124f2170417e8
Frcgg <sevillanorichar266@gmail.com>
2025-04-10 22:16:44 -0500

    navbar modificado

--- proyecto-pasto/frontend/src/App.jsx
+++ proyecto-pasto/frontend/src/App.jsx
@@ -10,13 +10,14 @@ import InfoContacto from './components/GuiaTuristico/InfoContacto';
 //import Restaurantes from './components/GuiaTuristico/Restaurantes';
 //import RutasTuristicas from './components/GuiaTuristico/RutasTuristicas';
 import AdminPanel from './components/Admin/Adminpanel'; // Importa el componente AdminPanel
+import navbarBackground from "../public/Img_Video/navbar.jpg"; // Ajusta la ruta según tu estructura
 
 
 function App() {
   return (
-    
+
     <Router>
-      <nav className="navbar navbar-expand-lg navbar-light bg-light">
+      <nav className="navbar navbar-expand-lg navbar-light" style={{ backgroundImage: `url(${navbarBackground})`, backgroundSize: 'cover', backgroundPosition: 'center', color: 'white' }}>
         <div className="container-fluid">
           <div className="d-flex justify-content-center">
             <Link className="nav-link" to="/">Inicio</Link>
@@ -26,11 +27,11 @@ function App() {
           </button>
           <div className="collapse navbar-collapse" id="navbarNav">
             <ul className="navbar-nav ms-auto me-auto mb-2 mb-lg-0">
-              <li className="nav-item">
-                <Link className="nav-link" to="/sobre-pasto">Sobre Pasto</Link>
+              <li className="nav-item" >
+                <Link className="nav-link" to="/sobre-pasto"  style={{ color: 'white' }} >Sobre Pasto</Link>
               </li>
               <li className="nav-item">
-                <Link className="nav-link" to="/sobre-carnaval">Sobre el Carnaval</Link>
+                <Link className="nav-link" to="/sobre-carnaval" style={{ color: 'white' }}>Sobre el Carnaval</Link>
               </li>
               <li className="nav-item">
                 <Link className="nav-link" to="/guia-turistico"></Link>
@@ -41,19 +42,18 @@ function App() {
               <li className="nav-item">
                 <Link className="nav-link" to="/iniciar-sesion"></Link>
               </li>
-              
             </ul>
           </div>
-          
         </div>
       </nav>
+
 
       <Routes>
         <Route path="/sobre-pasto" element={<SobrePasto />} />
         <Route path="/sobre-carnaval" element={<SobreCarnaval />} />
         <Route path="/" element={<Introduccion />} />
         <Route path="/admin" element={<AdminPanel />} /> {/* Ruta para el AdminPanel */}
-        <Route path="guia-turistico" element={<InfoContacto/>} />
+        <Route path="guia-turistico" element={<InfoContacto />} />
 
       </Routes>
     </Router>
